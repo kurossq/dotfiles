20176fe2f537d4e7f52404fbaba104442ebc65a8
Ibuki-E <knight.free.ten@gmail.com>
2015-05-08 20:56:51 +0900

    Add proxy setting of Github

--- .init_ubuntu14.04.sh
+++ .init_ubuntu14.04.sh
@@ -64,6 +64,11 @@ sudo apt-get update
 # githubを使いたい人は以下のコメントを実行してください
 #git config --global http.proxy http://proxy.uec.ac.jp:8080
 #git config --global https.proxy http://proxy.uec.ac.jp:8080
+#echo Host github.com >> ~/.ssh/config
+#echo 	HostName ssh.github.com >> ~/.ssh/config
+#echo 	port 443 >> ~/.ssh/config
+#echo 	proxycommand nc -X connect -x proxy.uec.ac.jp:8080 %h %p >> ~/.ssh/config
+
 # vimの設定ファイルを引っ張ってくる
 git clone git@github.com:Ibuki-E/dotfiles.git
 ln -s ~/dotfiles/.vimrc ~/.vimrc
